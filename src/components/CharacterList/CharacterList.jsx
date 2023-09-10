6531a74f0578c98c36b3c69d4ae80a1f94fc09cd
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-09-10 12:44:26 -0500

    confirmed user character list is displaying in console log on character list component

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,5 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, {useEffect} from 'react';
 
 function CharacterList (){
+    const dispatch = useDispatch();
+    const charList = useSelector(store => store.charList)
+
+    useEffect(() => {
+    getCharList();
+    }, [])
+
+    const getCharList = () => {
+        dispatch({type: 'FETCH_CHARLIST'})
+    }
+    console.log(charList)
 
     return(
         <h1>Hooray in Character List</h1>
