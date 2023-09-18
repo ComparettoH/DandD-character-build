40434ead62210cf4fda96294e2ba7f7f44732ee1
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-09-18 09:41:40 -0500

    update style

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { Button } from '@mantine/core';
+import { Textarea } from '@mantine/core';
 
 
 export function Character() {
@@ -70,31 +71,31 @@ export function Character() {
     console.log('testing for character edit', charToEdit)
 
     return (
-        <div style={{backgroundColor:"black", color:"white"}} id="one-character">
+        <div style={{ backgroundColor: "black", color: "white" }} id="one-character">
             <h1>Edit Character</h1>
             <span>
-                    <form onSubmit={handleSubmit}>
-                        <>
-                                <Fragment key={charToEdit.id}>
-                                    <h2>Update Character's Name To:</h2>
-                                    <input key='1'
-                                        value={charToEdit.character_name}
-                                        placeholder='New Name'
-                                        onChange={(event) => handleUpdate(event, 'character_name')}
-                                    ></input>
+                <form onSubmit={handleSubmit}>
+                    <>
+                        <Fragment key={charToEdit.id}>
+                            <h2>Update Character's Name To:</h2>
+                            <input key='1'
+                                value={charToEdit.character_name}
+                                placeholder='New Name'
+                                onChange={(event) => handleUpdate(event, 'character_name')}
+                            ></input>
 
-                                    <p>Current Race: {charToEdit.race}</p>
-                                    {/* <Container>
+                            <p>Current Race: {charToEdit.race}</p>
+                            {/* <Container>
                                     <select key='4' size="4" multiple>
                                         {race.map((oneRace) =>
                                             <option onClick={(event) => handleUpdate(event, 'race')} key={oneRace.id} value={charToEdit.race}>{oneRace.race}</option>
                                         )}
                                     </select>
                                     </Container> */}
-                                    
 
-                                    <p>Current Class: {charToEdit.class}</p>
-                                    {/* <Container>
+
+                            <p>Current Class: {charToEdit.class}</p>
+                            {/* <Container>
                                     <select key='3' size="4" multiple>
                                         {classR.map((oneClass) =>
                                             <option onClick={() => handleUpdate(event, 'character_class')} key={oneClass.id} value={charToEdit.class}>{oneClass.class}</option>
@@ -102,24 +103,29 @@ export function Character() {
                                     </select>
                                     </Container> */}
 
-                                    <p>Current Background: {charToEdit.background}</p>
-                                    {/* <select size="4" multiple>
+                            <p>Current Background: {charToEdit.background}</p>
+                            {/* <select size="4" multiple>
                                         {background.map((oneBG) =>
                                             <option onClick={() => handleUpdate(event, 'character_background')} key={oneBG.id} value={charToEdit.character_background}>{oneBG.background}</option>
                                         )}
                                     </select> */}
 
-                                    <h2>Update Character Backstory:</h2>
-                                    <textarea key='2'
-                                        value={charToEdit.character_backstory}
-                                        placeholder='New Story'
-                                        onChange={(event) => handleUpdate(event, 'character_backstory')} />
-                                </Fragment>
-                            <br></br>
-                            <Button  color="dark" type='submit'>Update Character</Button>
-                            <Button  color="red" style={{margin: '10px'}} onClick={cancelEdit}>Cancel</Button>
-                        </>
-                    </form>
+                            <h2>Update Character Backstory:</h2>
+                            <Textarea
+                                aria-label="My textarea"
+                                placeholder='Enter backstory, questions for your DM, and/or thoughts here'
+                                size='xs'
+                                px='xl'
+                                minRows={2}
+                                maxRows={4}
+                                value={charToEdit.character_backstory}
+                                onChange={(event) => handleUpdate(event, 'character_backstory')} />
+                        </Fragment>
+                        <br></br>
+                        <Button color="dark" type='submit'>Update Character</Button>
+                        <Button color="red" style={{ margin: '10px' }} onClick={cancelEdit}>Cancel</Button>
+                    </>
+                </form>
             </span>
         </div>
 
