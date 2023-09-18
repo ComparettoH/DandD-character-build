679a6fd25f165565f66a709e3fe7240c82fe3b10
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-09-18 06:36:48 -0500

    more styling finally fixed character list render issue

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import React, {Fragment, useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 import './CharacterList.css';
+import { Container, Button } from '@mantine/core';
 
 function CharacterList (){
     const dispatch = useDispatch();
@@ -37,38 +38,27 @@ function CharacterList (){
 
     return(
         <>
-        <h1 style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100%"
-          }}>{user.username}'s Character List</h1>
-        <div id='user-list'
-        style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100%",
-            backgroundColor: 'black',
-            margin: "15%",
-          }}>
-        <span>
+        <h1 className='list-header'>{user.username}'s Character List</h1>
+        <div id='user-list'>
+        <Container className='char'>
         {charList.map((char) => 
         <Fragment key={char.id}>
             <h1>{char.character_name}</h1>
-            <p>{char.race}</p>
-            <p>{char.class}</p>
-            <p>{char.background}</p>
-            <p>{char.character_backstory}</p>
-            <button onClick={() => goToEdit(char)}>
+            <p>Character Race: {char.race}</p>
+            <p>Character Class: {char.class}</p>
+            <p>Character Background: {char.background}</p>
+            <p>Character Backstory:
+                <br></br>
+                {char.character_backstory}</p>
+            <Button color="lime" onClick={() => goToEdit(char)}>
                 Edit
-            </button> 
-            <button onClick={() => handleDelete(char)}>
+            </Button> 
+            <Button color="red"  style={{margin: '10px'}} onClick={() => handleDelete(char)}>
                Remove
-            </button>
+            </Button>
             </Fragment>
         )}
-        </span>
+        </Container>
         </div>
         </>
     )
